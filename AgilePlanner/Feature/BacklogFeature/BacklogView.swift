ed5feddae0b726bd0eb7603bc6eaf0ecdcc679bd
kouki-shito <likegameskou@gmail.com>
2026-04-23 17:43:08 +0900

    feat: TabViewの準備とファイル名重複エラーの修正 SMP-33

--- AgilePlanner/Feature/BacklogFeature/BacklogView.swift
+++ AgilePlanner/Feature/BacklogFeature/BacklogView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  BacklogView.swift
 //  AgilePlanner
 //
 //  Created by 市東 on 2026/04/22.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct BacklogView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    BacklogView()
 }
